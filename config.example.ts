5f20a1361acf44c34153a8f76518de0abe5374a3
maxmazal <maxmazal@udel.edu>
2024-12-08 18:32:28 -0500

    removed commented code

--- config.example.ts
+++ config.example.ts
@@ -99,43 +99,7 @@ export const config: Config = {
 	SEMESTER_ID: getEnvVar('SEMESTER_ID'),
 	BLACKLIST: getEnvVar('BLACKLIST')
 };
-/*
-export const { DB_CONNECTION } = process.env.DB_CONNECTION;
-export const { BOT_NAME } = process.env;
-export const { BOT_TOKEN } = process.env;
-export const { BOT_CLIENT_ID } = process.env;
-export const { DB_USERS } = process.env;
-export const { DB_PVQ } = process.env;
-export const { DB_QTAGS } = process.env;
-export const { DB_ASSIGNABLE } = process.env;
-export const { DB_COURSES } = process.env;
-export const { DB_REMINDERS } = process.env;
-export const { DB_CLIENT_DATA } = process.env;
-export const { DB_POLLS } = process.env;
-export const { GUILD_MAIN } = process.env;
-export const { GUILD_GATEWAY } = process.env;
-export const { GUILD_GATEWAY_INVITE } = process.env;
-export const { ROLE_ADMIN } = process.env;
-export const { ROLE_STUDENT_ADMIN } = process.env;
-export const { ROLE_STAFF } = process.env;
-export const { ROLE_VERIFIED } = process.env;
-export const { ROLE_MUTED } = process.env;
-export const { ROLE_LEVEL_ONE } = process.env;
-export const { EMAIL_SENDER } = process.env;
-export const { EMAIL_REPLY_TO } = process.env;
-export const { EMAIL_REPORT_ADDRESSES } = process.env;
-export const { CHANNEL_ERROR_LOG } = process.env;
-export const { CHANNEL_SERVER_LOG } = process.env;
-export const { CHANNEL_MEMBER_LOG } = process.env;
-export const { CHANNEL_MOD_LOG } = process.env;
-export const { CHANNEL_FEEDBACK } = process.env;
-export const { CHANNEL_SAGE } = process.env;
-export const { CHANNEL_ANNOUNCEMENTS } = process.env;
-export const { CHANNEL_ARCHIVE } = process.env;
-export const { CHANNEL_ROLE_SELECT } = process.env;
-export const { ROLE_DROPDOWNS_COURSE_ROLES } = process.env;
-export const { ROLE_DROPDOWNS_ASSIGN_ROLES } = process.env;
-*/
+
 
 export const BOT = {
 	TOKEN: config.BOT_TOKEN,
